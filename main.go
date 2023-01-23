15475f05d970215310974e72c558baeba47dc09f
Donnukrit S <kalima007c@gmail.com>
2023-01-23 16:46:22 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+
+	e := echo.New()
+	e.GET("/", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "Hello World")
+	})
+	e.Logger.Fatal(e.Start(":3070"))
+}
